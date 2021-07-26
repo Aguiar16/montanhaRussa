f72ec3dca594b1d09b353fad9774f55f301a76e1
Matheus Varela <matheus.varela@aluno.uece.br>
2021-07-26 00:15:13 -0300

    changing

--- roadrollaDAH.py
+++ roadrollaDAH.py
@@ -1,54 +1,54 @@
 import random,random,time,threading,random,statistics
 
-npassageiros = 0
-ncarros = 0
+nPassengers = 0
+nCars = 0
 
-# Semaforos/mutex do carro/passageiro
+# Semaforos/mutex do Car/Passenger
 vaga = threading.Semaphore(0)
 vaza = threading.Semaphore(0)
 mutex = threading.Lock() 
 embarque = threading.Semaphore(1)
 
-# variavel para os carros
-temposC = []
-# variavel para os passageiros
+# Storing Cars and Passengers time
+timeCars = []
+timePassengers = []
+# variavel para os passengers
 passId = 1
 fila = 0
 sentados = threading.Semaphore(0)
 vazaro = threading.Semaphore(0)
 filaVazia = False
-temposP = []
 
-class MontanhaRussa:
+class RollerCoaster:
 
-    def __init__(self,ncarros,npassageiros):
-        self.passageiros = npassageiros
-        self.totalCarros = ncarros
-        self.carrosId = 1
+    def __init__(self, nCars, nPassengers):
+        self.passengers = nPassengers
+        self.totalOfCars = nCars
+        self.carsId = 1
         self.threadsC = []
         self.threadsP = []
 
-    def criaCarros(self):
-        for i in range(self.totalCarros):
-            c = threading.Thread(target=Carro(self.carrosId).start)
-            self.carrosId +=1
+    def spawnCars(self):
+        for i in range(self.totalOfCars):
+            c = threading.Thread(target=Car(self.carsId).start)
+            self.carsId +=1
             self.threadsC.append(c)
 
-    def criaPassageiro(self):
+    def spawnPassenger(self):
         global fila
-        for i in range(self.passageiros):
-            c = threading.Thread(target=Passageiro(i+1).start)
+        for i in range(self.passengers):
+            c = threading.Thread(target=Passenger(i+1).start)
             self.threadsP.append(c)
 
     def run(self):
-        self.criaCarros()
-        self.criaPassageiro()
+        self.spawnCars()
+        self.spawnPassenger()
         for t in self.threadsC:
             t.start()
 
         for t in self.threadsP:
             t.start()
-            zzz = random.choice([1,2,3])
+            zzz = random.choice([1, 2, 3])
             time.sleep(zzz)
         
         for t in self.threadsC:
@@ -57,17 +57,17 @@ class MontanhaRussa:
         for t in self.threadsP:
             t.join()
         
-        print('A montanha russa esta fechando ...\n')
+        print('The roller coaster is closing up...\n')
 
         with open('statistics.txt','a') as output:
-            output.write("\nmedia de tempo na fila dos passageiros: "+ str(statistics.mean(temposP))+"\n")
-            output.write("maximo de tempo na fila dos passageiros: "+ str(max(temposP))+"\n")
-            output.write("minimo de tempo na fila dos passageiros: "+ str(min(temposP))+"\n")
-            output.write("taxa de utilização dos carros: "+ str(temposC)+"\n")
+            output.write("\nTempo médio dos passageiros na fila: "+ str(statistics.mean(timePassengers))+"\n")
+            output.write("Tempo máximo de espera dos passageiros na fila: "+ str(max(timePassengers))+"\n")
+            output.write("Tempo mínimo de espera dos passageiros na fila: "+ str(min(timePassengers))+"\n")
+            output.write("Taxa de utilização dos carros: "+ str(timeCars)+"\n")
         
-        print('Adeus\n')
+        print('Farewell\n')
 
-class Passageiro:
+class Passenger:
     def __init__ (self,id):
         self.id = id
         self.tfila = 0
@@ -76,9 +76,9 @@ class Passageiro:
     def board(self):
         global filaVazia
         global passId
-        global temposP
+        global timePassengers
         global fila
-        print(f'Passageiro {self.id} esperando na fila')
+        print(f'Passenger {self.id} esperando na fila')
         self.tfila = time.time()
         with mutex:
             fila +=1
@@ -86,12 +86,12 @@ class Passageiro:
         while not self.sentado:
             if self.id == passId:
                 vaga.acquire()
-                temposP.append((time.time()-self.tfila))
-                print(f'Passageiro {self.id} entrando no carro')
+                timePassengers.append((time.time()-self.tfila))
+                print(f'Passenger {self.id} entrando no Car')
                 self.sentado = True
                 with mutex:
                     fila -=1
-                if self.id == npassageiros:
+                if self.id == nPassengers:
                     filaVazia = True
 
                 if (self.id)%4 == 0:
@@ -100,14 +100,14 @@ class Passageiro:
     
     def unboard(self):
         vaza.acquire()
-        print(f'Passageiro {self.id} esta saindo do carro')
+        print(f'Passenger {self.id} esta saindo do Car')
         vazaro.release()
 
     def start(self):
         self.board()
         self.unboard()
         
-class Carro:
+class Car:
     def __init__ (self,nome):
         self.capacidade = 4
         self.nome = nome
@@ -117,7 +117,7 @@ class Carro:
 
     def load(self):
         self.ocupado.acquire()
-        print(f"\nCarro {self.nome} pronto para o embarque de passageiros\n")
+        print(f"\nCar {self.nome} pronto para o embarque de passengers\n")
         while not filaVazia:
             if fila > 3:
                 for i in range(self.capacidade):
@@ -129,13 +129,13 @@ class Carro:
     
     def run(self):
         self.ocupado.acquire()
-        print(f"\nCarro {self.nome} esta em movimento.\n")
+        print(f"\nCar {self.nome} esta em movimento.\n")
         time.sleep(10)
         self.ocupado.release()
 
     def unload(self):
         self.ocupado.acquire()
-        print(f"\nCarro {self.nome} terminou o passeio.\n")
+        print(f"\nCar {self.nome} terminou o passeio.\n")
         for i in range(self.capacidade):
             vaza.release()
             vazaro.acquire()
@@ -144,7 +144,7 @@ class Carro:
         
     def start (self):
         
-        global temposC
+        global timeCars
         global filaVazia
         embarque.acquire()
         while not filaVazia:
@@ -156,30 +156,37 @@ class Carro:
             self.unload()
             embarque.acquire()
         embarque.release()
-        print(f"\nCarro {self.nome} esta sendo desligado.\n")
+        print(f"\nCar {self.nome} esta sendo desligado.\n")
         self.tTotal = time.time() - self.tTotal
         with mutex:
-            temposC.append(self.tCorrida/self.tTotal)
+            timeCars.append(self.tCorrida/self.tTotal)
 
 def main ():
-    global ncarros
-    global npassageiros
-    c = input('1 - 1 carro, 52 passageiros, 2- 2 carros e 92 passageiros, 3- 3 carros e 148 passageiros\n')
+    global nCars
+    global nPassengers
+    print('This is a roller coaster simulation using parallel programming \n \n')
+    c = input('Please type:\n [1] for [1 car, 52 passengers] \n [2] for [2 cars and 92 passengers] \n [3] for [3 cars and 148 passengers]\n')
+    
     if c == '1':
-        ncarros = 1
-        npassageiros = 52
-        d = MontanhaRussa(ncarros,npassageiros)
-        d.run()
+        nCars = 1
+        nPassengers = 52
+        rollerCoaster = RollerCoaster(nCars,nPassengers)
+        rollerCoaster.run()
+    
     elif c == '2':
-        ncarros = 2
-        npassageiros = 92
-        d = MontanhaRussa(ncarros,npassageiros)
-        d.run()
+        nCars = 2
+        nPassengers = 92
+        rollerCoaster = RollerCoaster(nCars,nPassengers)
+        rollerCoaster.run()
+    
     elif c == '3':
-        ncarros = 3
-        npassageiros = 148
-        d = MontanhaRussa(ncarros,npassageiros)
-        d.run()
+        nCars = 3
+        nPassengers = 148
+        rollerCoaster = RollerCoaster(nCars,nPassengers)
+        rollerCoaster.run()
+    else:
+        print('\nWrong input! \nPlease type only 1, 2 or 3\n')
+        main()
 
 if __name__ == "__main__":
     main()
